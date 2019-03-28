7c14c4db8e986560416028271ee84ca259b5bd08
libin <libin2@ctsi.com.cn>
2019-03-28 10:29:36 +0800

    swagger 的集成

--- src/main/java/com/ctsi/springboot/tomcat/controller/HelloWorldController.java
+++ src/main/java/com/ctsi/springboot/tomcat/controller/HelloWorldController.java
@@ -2,6 +2,7 @@ package com.ctsi.springboot.tomcat.controller;
 
 import org.apache.log4j.Logger;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,6 +14,11 @@ public class HelloWorldController {
 	String home() {
 		log.info("## Welcome");
 		return "Welcome You";
+	}
+	
+	@RequestMapping(value = "/index", method = RequestMethod.GET)
+	String index(String name) {
+		return "Hello " + name;
 	}
 
 }
